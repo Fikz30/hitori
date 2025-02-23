11987fea2219c97ab10358d8e7ce98d371e8d1bc
Fikz30 <gduffugohi@gmail.com>
2025-02-23 11:54:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,17 +2,17 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 /*
-	* Create By Naze
-	* Follow https://github.com/nazedev
-	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
+	* Create By Fikz
+	* Follow https://github.com/Fikz30
+	* Whatsapp : https://wa.me/6282189966179
 */
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner
+global.owner = ['6282189966179','6287829995313'] //['628','628'] 2 owner
 global.packname = 'Bot WhatsApp'
 global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.botname = 'Fikz Bot'
 global.listprefix = ['+','!','.']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
@@ -28,10 +28,7 @@ global.fake = {
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/B5qJIwZHm4VEYZJQE6iMwy',
-	ch: '120363250409960161@newsletter',
+       wa : https://wa.me/6282189966179
 }
 
 global.limit = {
